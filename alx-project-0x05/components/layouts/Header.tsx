7af040121543589694825b21d2a6d6289a433e92
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-12-06 18:06:12 +0000

    Implement CountContext for state management; refactor Header component to utilize context for count display and update _app.tsx to include CountProvider

--- alx-project-0x05/components/layouts/Header.tsx
+++ alx-project-0x05/components/layouts/Header.tsx
@@ -1,33 +1,38 @@
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { RootState } from "@/store/store";
-import { useSelector } from "react-redux";
+import { useCount } from "@/context/CountContext";
 
 const Header: React.FC = () => {
-  const pathname = usePathname();
-  const count = useSelector((state: RootState) => state.counter.value);
+
+  const pathname = usePathname()
+  const { count } = useCount()
 
   return (
     <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link
-          href="/"
-          className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight"
-        >
+        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
           Splash App
         </Link>
 
         {/* Button Group */}
         <div className="flex gap-4">
-          {!["/counter-app"].includes(pathname) ? (
-            <>
-              <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
-              <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
-            </>
-          ) : (
-            <p className=" font-semibold text-lg">Current count : {count}</p>
-          )}
+          {
+            !["/counter-app"].includes(pathname) ? (
+              <>
+              <Button
+            buttonLabel="Sign In"
+            buttonBackgroundColor="red"
+          />
+          <Button
+            buttonLabel="Sign Up"
+            buttonBackgroundColor="blue"
+          /></>
+            ) : (
+              <p className=" font-semibold text-lg">Current count : {count}</p>
+            )
+          }
+
         </div>
       </div>
     </header>
